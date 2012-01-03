ffea79a8cf49dbf195de99708bb8e1dcc0171fef
Mark Visser <mjmvisser@gmail.com>
2012-01-03 13:09:49 -0500

    add default _3DFM_SL_INCLUDE_PATH if not set

--- etc/init_env.sh
+++ etc/init_env.sh
@@ -5,13 +5,18 @@ etc_dir=$(cd "$etc_dir"; pwd)
 maya_version=$(maya -v | head -1 | awk '{print $2}')
 delight_version=$(( renderdl -v 2>&1 ) | head -1 | awk '{print $3}')
 
-export DLDIR=$(dirname $etc_dir)
+if [ -n "${_3DFM_SL_INCLUDE_PATH:-x}" ]; then
+    # set some sane defaults
+    export _3DFM_SL_INCLUDE_PATH=$DELIGHT/maya/rsl
+fi
 
-export PYTHONPATH=$DLDIR/python:$DLDIR/3rdparty/lib/python2.6/site-packages:$PYTHONPATH
-export PATH=$DLDIR/python/scripts:$DLDIR/3rdparty/bin:$PATH
+export DELUXE=$(dirname $etc_dir)
 
-export MAYA_SCRIPT_PATH=$DLDIR/maya/scripts:$DLDIR/3dfm/$delight_version/maya/$maya_version/scripts:$MAYA_SCRIPT_PATH
-export MAYA_PLUG_IN_PATH=$DLDIR/maya/plug-ins:$DLDIR/python/shaders:$MAYA_PLUG_IN_PATH
-export XBMLANGPATH=$DLDIR/maya/icons:$DLDIR/python/shaders:$XBMLANGPATH
+export PYTHONPATH=$DELUXE/python:$DELUXE/3rdparty/lib/python2.6/site-packages:$PYTHONPATH
+export PATH=$DELUXE/python/scripts:$DELUXE/3rdparty/bin:$PATH
 
-export _3DFM_SL_INCLUDE_PATH=$DLDIR/python/shaders:$DLDIR/python/shaders/include:$DLDIR/python/shaders/include:$DLDIR/shadeops:$_3DFM_SL_INCLUDE_PATH
+export MAYA_SCRIPT_PATH=$DELUXE/maya/scripts:$DELUXE/3dfm/$delight_version/maya/$maya_version/scripts:$MAYA_SCRIPT_PATH
+export MAYA_PLUG_IN_PATH=$DELUXE/maya/plug-ins:$DELUXE/python/shaders:$MAYA_PLUG_IN_PATH
+export XBMLANGPATH=$DELUXE/maya/icons:$DELUXE/python/shaders:$XBMLANGPATH
+
+export _3DFM_SL_INCLUDE_PATH=$DELUXE/python/shaders:$DELUXE/python/shaders/include:$DELUXE/python/shaders/include:$DELUXE/shadeops:$_3DFM_SL_INCLUDE_PATH
